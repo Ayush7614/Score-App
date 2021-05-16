9d0304d87dad39270c098052f7383b7bcc6b82b7
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-05-16 10:53:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+/* Author - Ayush Kumar*/
+
+function scoreTeamBlue(val) {
+    var score = document.getElementById('blue').value;
+    var new_score = parseInt(score,10) + val;
+    
+    if (new_score < 0) {
+        new_score = 0;
+    }
+    
+    var path='audio/stereo_hearts.mp3';
+    var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', path);
+  audioElement.play();
+   document.getElementById('blue').value = new_score;
+    return new_score;
+    
+}
+
+function scoreTeamRed(val) {
+    var score = document.getElementById('red').value;
+    var new_score = parseInt(score,10) + val;
+    
+    if (new_score < 0) {
+        new_score = 0;
+    }
+    
+    var path='audio/stereo_hearts.mp3';
+    var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', path);
+  audioElement.play();
+   document.getElementById('red').value = new_score;
+    return new_score;
+}
+
+function reset(val){
+    var path='audio/stereo_hearts.mp3';
+    var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', path);
+  audioElement.play();
+    document.getElementById('red').value=0;
+    document.getElementById('blue').value=0;   
+}
+
+/* Made by Ayush Kumar */
